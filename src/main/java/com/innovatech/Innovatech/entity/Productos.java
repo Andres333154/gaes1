759482203c2894961ca5056fd0c76c7f46c0b50d
harold <haroldurbanoht@gmail.com>
2022-10-28 14:37:38 -0500

    proyecto

---
+++ src/main/java/com/innovatech/Innovatech/entity/Productos.java
@@ -0,0 +1,13 @@
+package com.innovatech.Innovatech.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+@Getter
+@Setter
+public class Productos {
+}
---
+++ src/main/java/com/innovatech/Innovatech/entity/User.java
@@ -0,0 +1,26 @@
+package com.innovatech.Innovatech.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "rol")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idRoll", unique = true, nullable = false)
+    private Long id;
+
+    @Column(length = 50)
+
+    private String name;
+
+    @Column(length = 50, name = "last_name")
+    private String lastName;
+
+    @Column(length = 50)
+    private String password;
+}
